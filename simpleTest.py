cb367018bb08b5dc9a58dc7b1bb1982cf4ce2d56
别忘羊皮 <124776240+lllyhLUO@users.noreply.github.com>
2025-12-29 16:52:24 +0800

    Update simpleTest.py

--- simpleTest.py
+++ simpleTest.py
@@ -13,3 +13,17 @@ def main():
 
     assert response.status_code == 200
     assert 'baidu' in response.text
+
+# 测试表单
+user_info = {
+    'name': 'username',
+    'password': '123456',
+}
+def test_api_form():
+
+    resp = requests.request(
+        method='post',
+        url='https://httpbin.org/post', # 要保证这个网址是能够接受user_info字典格式的post请求才能通过
+        data=user_info #就不用设置请求头了
+    )
+    assert resp.s
